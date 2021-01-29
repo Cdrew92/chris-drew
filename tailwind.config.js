215f1eb26fdb4ea73bda40f216c66004b6c4edb5
Cdrew92 <39171327+Cdrew92@users.noreply.github.com>
2021-01-29 08:59:11 -0600

    tailwind config - Purge styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+  purge: ['*.html',],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
